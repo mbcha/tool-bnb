e86f009db7226802e1a4b6f0c67fec0cf342b156
Bella Cha <cha.maria.belen@gmail.com>
2018-11-05 14:27:50 +0100

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
+  root to: 'listings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :listings, only: [:index, :new, :create, :edit, :upload] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :users, only: [:show, :destroy]
 end
