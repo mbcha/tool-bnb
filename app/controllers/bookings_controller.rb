39f5a675658dab0bf194ef79608ac63daa7e5ac7
maxime fontana <maximefontana@hotmail.com>
2018-11-05 14:53:51 +0100

    new, create methods in controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,24 @@
+class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.listing = Listing.find(params[:listing_id])
+    @booking.user = User.find(params[:user_id])
+
+    if @booking.save
+      redirect_to listings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+end
