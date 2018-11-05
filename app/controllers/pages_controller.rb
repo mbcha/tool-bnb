20742ac9dafc7286ccf3fa6c949dbf4a97a91b9d
Bella Cha <cha.maria.belen@gmail.com>
2018-11-05 16:39:27 +0100

    fixed error message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ActionController::Base
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home], raise: false
 
   def home
   end
