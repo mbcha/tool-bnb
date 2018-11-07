9a60f347b6ea246974774f6961f4c650970bf66a
Bella Cha <cha.maria.belen@gmail.com>
2018-11-07 13:34:23 +0100

    add listing status

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,6 +7,7 @@ class BookingsController < ApplicationController
     @booking = Booking.create(booking_params)
     authorize @booking
     @booking.listing = Listing.find(params[:listing_id])
+    @booking.listing.available = false
     @booking.user = current_user
     @booking.total_price = @booking.listing.price * (@booking.end_date - @booking.start_date)
 
