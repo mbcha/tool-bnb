ae1aabad2e90b8835b1be68945e795326a48724d
Bella Cha <cha.maria.belen@gmail.com>
2018-11-08 18:27:23 +0100

    changed style

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2018_11_08_154133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.float "latitude"
+    t.float "longitude"
     t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
